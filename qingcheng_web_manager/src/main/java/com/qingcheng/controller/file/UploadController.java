02e40df52b1feaee71545595158ff872fad3eafb
Hou Ruihao <1830463600@qq.com>
2020-08-08 08:25:40 +0800

    1.添加本地图片上传 2.添加OSS配置，实现对象存储OSS-阿里云

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
@@ -1,5 +1,8 @@
 package com.qingcheng.controller.file;
 
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author houruihao
@@ -23,6 +29,14 @@ public class UploadController {
     @Autowired
     private HttpServletRequest request;
 
+    @Autowired
+    private OSSClient ossClient;
+
+    /**
+     * 本地上传
+     * @param file 文件对象
+     * @return 文件访问路径
+     */
     @PostMapping("/native")
     public String nativeUpload( @RequestParam("file") MultipartFile file){
 
@@ -39,5 +53,34 @@ public class UploadController {
             e.printStackTrace();
         }
         return "http://localhost:9101/img/"+file.getOriginalFilename();
+    }
+
+    /**
+     * 本地上传
+     * @param file 文件对象
+     * @return 文件访问路径
+     */
+    @RequestMapping("/oss")
+    public String ossUpload( @RequestParam("file") MultipartFile file, String folder){
+        //bucket名
+        String bucketName = "qingcheng-hou";
+        //定义文件名
+        String key =folder+"/"+ UUID.randomUUID() +file.getOriginalFilename();
+        PutObjectRequest putObjectRequest = null;
+        try {
+            putObjectRequest = new PutObjectRequest(bucketName, key, file.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ossClient.putObject(putObjectRequest);
+        // 设置URL过期时间为10年  3600l* 1000*24*365*10
+        Date expiration = new Date(System.currentTimeMillis()+ 3600L * 1000 * 24 * 365 * 10);
+        // 生成URL
+        String Rurl = ossClient.generatePresignedUrl(bucketName, key, expiration).toString();
+        String url = "";
+        if (!"".equals(Rurl)) {
+            url = Rurl;
+        }
+        return url;
     }
 }
